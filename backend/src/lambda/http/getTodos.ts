3723e81aaf16a6fb75dd92772d96f217c78efabb
MohamedGouaouri <im_gouaouri@esi.dz>
2023-02-20 12:42:24 +0100

    fix get todos function

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -3,47 +3,25 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
-import * as AWS from 'aws-sdk'
-// import * as AWSXRay from 'aws-xray-sdk'
-
-// const XAWS = AWSXRay.captureAWS(AWS)
-
-// import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-// import { getUserId } from '../utils';
-// import { getAllTodos } from '../../businessLogic/todos'
-// import { TodoItem } from '../../models/TodoItem'
 
 
-const docClient = new AWS.DynamoDB.DocumentClient()
-const todosTable = process.env.TODOS_TABLE
-// const todosIndex = process.env.TODOS_CREATED_AT_INDEX
+import { getUserId } from '../utils';
+import { getAllTodos } from '../../businessLogic/todos'
+import { TodoItem } from '../../models/TodoItem'
+
 
 // TODO: Get all TODO items for a current user
 // DONE
 export const handler = middy(
-  async (_: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
-    // const userId: string = getUserId(event)
-    // const todos: TodoItem[] = await getAllTodos(userId)
-    // const todos = [{ userId, todoId: 'todoId' }]
-    const result = await docClient.scan({
-      TableName: todosTable,
-      // IndexName: todosIndex,
-      // KeyConditionExpression: 'userId = :userId',
-      // ExpressionAttributeValues: {
-      //   ':userId': userId
-      // }
-    }).promise()
-
-    // logger.info("All todos fetched")
-
-    const items = result.Items
-    // return items as TodoItem[]
+    const userId: string = getUserId(event)
+    const todos: TodoItem[] = await getAllTodos(userId)
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        items: items,
+        items: todos,
       })
     }
   })
